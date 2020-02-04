bd3ff7bda12c2297fce8d2550b297ab26bb6d594
David Lau <David.lau@codeimmersives.com>
2020-02-04 14:57:16 -0500

    finished

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function fizzy(num) {
-
+if (num%3 === 0 && num%5 !== 0 ){
+  return 'Fizz'
+} else if (num%5 === 0 && num%3 !== 0){
+  return 'Buzz'
+} else if ( num%5 === 0 && num%3 ===0) {
+  return 'FizzBuzz'
+}else {
+  return num
+}
 }
 
 module.exports = {
   fizzy,
 }
+
+
